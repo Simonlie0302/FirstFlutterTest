23b23c6ae4b7acd409caa778ba45809a07579cbf
Simon Fladen Lie <simonfl@uia.no>
2021-04-06 10:16:39 +0200

    Added delete quote functionality

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -20,49 +21,29 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: "Test author", text: "3 test quote from test author")
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Awesome Quotes"),
         centerTitle: true,
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.lightGreenAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+          delete: (){
+              setState(() {
+                quotes.remove(quote);
+              });
+          },
+        )).toList(),
       ),
     );
   }
 }
+
+
 
 
